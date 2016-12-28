4fdb3b67fc87e2d1ebd03646751152110328817c
HughParsonage <hugh.parsonage@gmail.com>
2016-12-28 15:38:10 +1100

    Initial commit of rm_ed..brackets

---
+++ R/rm_editorial_square_brackets.R
@@ -0,0 +1,25 @@
+#' Remove editorial square brackets
+#' @description Change text such as \code{phas[e] out} to \code{phase out},
+#' without removing square brackets denoting optional arguments.
+#' @param tex_lines Lines (as from \code{readLines}).
+#' @export
+
+rm_editorial_square_brackets <- function(tex_lines){
+  # Assume square brackets are ordinary if they are not
+  # preceded by a } or ] or a word preceded by a backslash
+  gsub(paste0("(",
+              "(?:\\s+[A-Za-z]*)",
+              "|",
+              "(?:^[A-Za-z]*)",
+              "\\s*",
+              ")",
+              "\\[",
+              "(",
+              "(?:\\w|\\s|\\d)+",
+              ")",
+              "\\]"),
+       "\\1\\2",
+       tex_lines,
+       perl = TRUE)
+}
+

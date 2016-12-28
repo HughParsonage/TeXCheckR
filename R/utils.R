e377db7058d781c1be0d161e512d733885bd11ac
HughParsonage <hugh.parsonage@gmail.com>
2016-12-28 11:05:35 +1100

    Add `Seq_union` for `replace_LaTeX_argument`

--- R/utils.R
+++ R/utils.R
@@ -2,3 +2,16 @@
 `%notin%` <- Negate(`%in%`)
 
 not_length0 <- function(x) length(x) > 0
+
+# takes a vector of froms and tos and takes their union
+Seq_union <- function(x, y){
+  if (length(x) == 1L && length(y) == 1L){
+    seq.int(x, y)
+  } else {
+    if (length(x) != 1L && length(y) != 1L){
+      c(Vectorize(seq.default, vectorize.args = c("from", "to"))(x, y))
+    } else {
+      unlist(Vectorize(seq.default, vectorize.args = c("from", "to"))(x, y))
+    }
+  }
+}

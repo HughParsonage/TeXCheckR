0a99af977662b282ab27fe025793c6168e3e122f
HughParsonage <hugh.parsonage@gmail.com>
2017-01-15 12:30:27 +1100

    Add multiply-defd labels as error in check_log

--- R/check_log.R
+++ R/check_log.R
@@ -5,14 +5,19 @@ check_log <- function(path = ".", final = FALSE){
   if (length(log_files) != 1){
     stop("Path does not contain a single log file.")
   }
-  
+
   log_file <- readLines(log_files[[1]])
-  
+
   if (any(grepl("undefined references", log_file, fixed = TRUE))){
     stop(grep("undefined references",
               log_file,
               fixed = TRUE,
               value = TRUE)[[1]])
   }
+
+  if (any(grepl("LaTeX Warning: There were multiply-defined labels.", log_file, fixed = TRUE))){
+    stop("LaTeX Warning: There were multiply-defined labels.")
+  }
+
   invisible(NULL)
 }

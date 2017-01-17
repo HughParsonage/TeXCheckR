8e22ce14de5075525ded15d0bde15370eeb7efea
HughParsonage <hugh.parsonage@gmail.com>
2017-01-18 01:28:43 +1100

    Fix non-detection of error; add test

--- R/check_sentence_ending_periods.R
+++ R/check_sentence_ending_periods.R
@@ -1,14 +1,30 @@
 
 
-check_sentence_ending_periods <- function(filename){
-  lines <- readLines(filename)
-
-  lines2 <- lines[isR_line_in_knitr(lines)]
-
-  for (line in lines2){
-    if (grepl("[A-Z]\\.\\s+[A-Z]", line, perl = TRUE)){
-      cat(line)
-      stop("Sentence ending period?")
-    }
+check_sentence_ending_periods <- function(filename, .report_error){
+  if (missing(.report_error)){
+    .report_error <- function(...) report2console(...)
+  }
+  lines <- 
+    readLines(filename, encoding = "UTF-8", warn = FALSE) %>%
+    .[!isR_line_in_knitr(.)] %>%
+    gsub("((?<!(\\\\))%).*$", "", ., perl = TRUE) %>%
+    trimws
+  
+  if (any(grepl("[A-Z]\\.\\s+[A-Z]", lines, perl = TRUE)) || 
+      any(and(lag(grepl("[A-Z]\\.$", lines, perl = TRUE)),
+              grepl("^[A-Z]", lines, perl = TRUE)))){
+    
+    line_nos <- 
+      which(or(grepl("[A-Z]\\.\\s+[A-Z]", lines, perl = TRUE),
+               and(lag(grepl("[A-Z]\\.$", lines, perl = TRUE)),
+                   grepl("^[A-Z]", lines, perl = TRUE))))
+    
+    .report_error(line_no = line_nos[[1]], 
+                  context = lines[line_nos[[1]]], 
+                  error_message = paste0("Sentences which end with a capital letter ",
+                                         "need to be signalled with a sentence-ending period. (\\@.)"))
+    stop(paste0("Sentences which end with a capital letter ",
+                "need to be signalled with a sentence-ending period. (\\@.)"))
   }
 }
+
---
+++ tests/testthat/test_check_sentence_ending_periods.R
@@ -0,0 +1,5 @@
+context("Sentence-ending periods")
+
+test_that("Raises error when sentence-ending period exists", {
+  expect_error(check_sentence_ending_periods("sentence-ending-period-error.tex"))
+})

0690be2784612b878bfffc3de4392f1aabd9bf9c
HughParsonage <hugh.parsonage@gmail.com>
2017-01-20 12:38:28 +1100

    Closes #4

--- data-raw/put-correctly-spelled-words.R
+++ data-raw/put-correctly-spelled-words.R
@@ -3,6 +3,11 @@ correctly_spelled_words_txt <- readLines("./data-raw/correctly_spelled_words.txt
 correctly_spelled_words_txt <- correctly_spelled_words_txt[order(correctly_spelled_words_txt)]
 writeLines(correctly_spelled_words_txt, "./data-raw/correctly_spelled_words.txt")
 
-correctly_spelled_words <- correctly_spelled_words_txt
+CORRECTLY_SPELLED_WORDS_CASE_SENSITIVE <-
+  grep("[A-Z]", correctly_spelled_words_txt, value = TRUE)
 
-devtools::use_data(correctly_spelled_words, overwrite = TRUE)
+correctly_spelled_words <- setdiff(correctly_spelled_words_txt, CORRECTLY_SPELLED_WORDS_CASE_SENSITIVE)
+
+devtools::use_data(correctly_spelled_words,
+                   CORRECTLY_SPELLED_WORDS_CASE_SENSITIVE,
+                   overwrite = TRUE)

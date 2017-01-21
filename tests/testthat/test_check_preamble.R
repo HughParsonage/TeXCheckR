ef7b0ca4a176b2e0ad1821c9e7ba555611d02c71
HughParsonage <hugh.parsonage@gmail.com>
2017-01-21 15:29:04 +1100

    check_preamble only errors here if final=T

--- tests/testthat/test_check_preamble.R
+++ tests/testthat/test_check_preamble.R
@@ -1,7 +1,7 @@
 context("check preamble")
 
 test_that("todonotes even if disabled not acceptable", {
-  expect_error(check_preamble("./check-preamble/todonotes.tex"))
-  expect_error(check_preamble("./check-preamble/todonotes-disable.tex"))
+  expect_error(check_preamble("./check-preamble/todonotes.tex", final = TRUE))
+  expect_error(check_preamble("./check-preamble/todonotes-disable.tex", final = TRUE))
   cat("\n")
 })

0b5c7120e6aafb1d7ba0585e3486fddb4f474c7b
HughParsonage <hugh.parsonage@gmail.com>
2017-01-22 20:00:02 +1100

    Tidy /testthat/ move to own dir

--- tests/testthat/test_cites.R
+++ tests/testthat/test_cites.R
@@ -1,9 +1,9 @@
 context("Citation formatting")
 
 test_that("Postnotes detected", {
-  expect_error(check_cite_pagerefs("cite-no-postnote.tex"), regexp = "postnote")
-  expect_error(check_cite_pagerefs("cite-p-in-postnote.tex"), regexp = "p in postnote")
-  expect_error(check_cite_pagerefs("cite-pp-in-postnote.tex"), regexp = "p in postnote")
+  expect_error(check_cite_pagerefs("./check-cite-pagerefs/cite-no-postnote.tex"), regexp = "postnote")
+  expect_error(check_cite_pagerefs("./check-cite-pagerefs/cite-p-in-postnote.tex"), regexp = "p in postnote")
+  expect_error(check_cite_pagerefs("./check-cite-pagerefs/cite-pp-in-postnote.tex"), regexp = "p in postnote")
 })
 
 test_that("Tweetable check_cite", {

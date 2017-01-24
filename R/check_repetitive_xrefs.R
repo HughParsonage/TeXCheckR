caeee94f4e3880c5768cf55550fe855fb1744eb8
HughParsonage <hugh.parsonage@gmail.com>
2017-01-24 11:19:37 +1100

    Check xrefs for Vref and Cref case-sensitive

--- R/check_repetitive_xrefs.R
+++ R/check_repetitive_xrefs.R
@@ -1,13 +1,32 @@
-#' Check repetitive cross-references
+#' Check cross-references
+#' @description Check cross-references that are repetitive or incorrect case.
 #' @param filename A LaTeX file
 #' @param .report_error The function to provide context to the error.
 #' @return NULL
 
-check_repetitive_xrefs <- function(filename, .report_error){
+check_xrefs <- function(filename, .report_error){
   if (missing(.report_error)){
     .report_error <- function(...) report2console(...)
   }
   lines <- readLines(filename, encoding = "UTF-8", warn = FALSE)
+  
+  if (any(grepl("\\\\[cv]ref\\{", lines, perl = TRUE, ignore.case = FALSE))){
+    if (any(grepl("\\\\cref\\{", lines, perl = TRUE, ignore.case = FALSE))){
+      line_no <- grep("\\\\cref\\{", lines, perl = TRUE, ignore.case = FALSE)[[1]]
+      context <- lines[[line_no]]
+      .report_error(line_no = line_no,
+                    context = context,
+                    error_message = "Lower-case \\Cref used. \\Vref and \\Cref are case-sensitive and the lower-case forms are not permitted.")
+      stop("Lower-case \\Cref used. \\Vref and \\Cref are case-sensitive and the lower-case forms are not permitted.")
+    } else {
+      line_no <- grep("\\\\vref\\{", lines, perl = TRUE, ignore.case = FALSE)[[1]]
+      context <- lines[[line_no]]
+      .report_error(line_no = line_no,
+                    context = context,
+                    error_message = "Lower-case \\Cref used. \\Vref and \\Cref are case-sensitive and the lower-case forms are not permitted.")
+      stop("Lower-case \\Vref used. \\Vref and \\Cref are case-sensitive and the lower-case forms are not permitted.")
+    }
+  }
 
   line_nos_with_VrefCref <- grep("[CVcv]ref", lines, perl = TRUE)
 

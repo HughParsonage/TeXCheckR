3dfe2926e65dc519bf025211d1563c81a155d1d2
HughParsonage <hugh.parsonage@gmail.com>
2017-01-26 18:05:49 +1100

    Fix: Errors only thrown with non-defaults

--- tests/testthat/test_check_all_figs_tbls_refd.R
+++ tests/testthat/test_check_all_figs_tbls_refd.R
@@ -1,13 +1,13 @@
 context("check all figs tbls refd")
 
 test_that("Error when not refd", {
-  expect_error(check_all_figs_tbls_refd("./check-all-figs-tbls-refd/fig_not_refd.tex"))
+  expect_error(check_all_figs_tbls_refd("./check-all-figs-tbls-refd/fig_not_refd.tex", compile = TRUE, pre_release = TRUE))
 })
 
 test_that("Error when not labeled", {
-  expect_error(check_labels("./check-all-figs-tbls-refd/fig-not-labeled.tex"))
+  expect_error(check_labels("./check-all-figs-tbls-refd/fig-not-labeled.tex", compile = TRUE, pre_release = TRUE))
 })
 
 test_that("No error when commented out", {
-  expect_null(check_all_figs_tbls_refd("./check-all-figs-tbls-refd/ok-as-fig-in-comment.tex"))
+  expect_null(check_all_figs_tbls_refd("./check-all-figs-tbls-refd/ok-as-fig-in-comment.tex", compile = TRUE, pre_release = TRUE))
 })

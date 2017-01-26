3f47230098992c4c1c0fc2b996500e89507b7a24
HughParsonage <hugh.parsonage@gmail.com>
2017-01-26 21:13:16 +1100

    Check ellipsis detection

--- tests/testthat/test_check_escapes.R
+++ tests/testthat/test_check_escapes.R
@@ -2,4 +2,5 @@ context("Check escapes")
 
 test_that("Dollar signs are not allowed.", {
   expect_error(check_escapes("./check-escapes/fail.tex"))
+  expect_error(check_escapes("./check-escapes/fail-2.tex"))
 })

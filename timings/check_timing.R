d2e4722d4cfb03ff6a29665deccf4b4f21d62c64
HughParsonage <hugh.parsonage@gmail.com>
2017-01-29 22:48:33 +1100

    Add new words, timings

--- timings/check_timing.R
+++ timings/check_timing.R
@@ -1,14 +1,16 @@
 
-check_timing <- function(comment = ""){
+check_timing <- function(comment = "", date = Sys.time()){
   stopifnot(dir.exists("./timings/"), 
-            dir.exists("./tests/testthat/SchoolFunding/"))
+            dir.exists("./tests/testthat/SchoolFunding/"), 
+            requireNamespace("data.table", quietly = TRUE))
+  library(data.table)
   checkCircuiBreaker <- function(){
     checkGrattanReport("./tests/testthat/SchoolFunding/")
   }
   timed <- microbenchmark::microbenchmark(checkCircuiBreaker(), times = 30L)
   timed %>%
     setDT %>%
-    .[, date := Sys.time()] %>%
+    .[, date := date] %>%
     .[, comment := comment] %>%
     fwrite("./timings/checkGrattanReport.csv", append = TRUE)
 }

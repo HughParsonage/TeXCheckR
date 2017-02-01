e76a9874320e3d92b0e3d1936b36e4c6932a4d45
HughParsonage <hugh.parsonage@gmail.com>
2017-02-01 13:13:01 +1100

    Fix: for loop creates line_no as side-effect

--- R/check_cite_pagerefs.R
+++ R/check_cite_pagerefs.R
@@ -9,28 +9,40 @@ check_cite_pagerefs <- function(filename, .report_error){
   line_nos_with_cites <-
     grep("cites?[\\[\\{]", lines, perl = TRUE)
 
-  for (line_no in line_nos_with_cites){
-    line <- lines[[line_no]]
-    if (grepl("cites?\\[[^\\]]+\\][{]", line, perl = TRUE)){
-      .report_error(line_no = line_no,
-                    context = line,
-                    error_message = "Use postnote for pagerefs.")
-      stop("Use postnote for pagerefs.")
-    }
+  if (any(grepl("cites?\\[[^\\]]+\\][{]", lines[line_nos_with_cites], perl = TRUE))){
+    line_no <- line_nos_with_cites[grepl("cites?\\[[^\\]]+\\][{]", lines[line_nos_with_cites], perl = TRUE)][[1]]
+    context <- lines[[line_no]]
+    .report_error(line_no = line_no,
+                  context = line,
+                  error_message = "Use postnote for pagerefs.")
+    stop("Use postnote for pagerefs.")
+  }
 
     # Check constructions like p. 93
-    if (grepl(paste0("cites?\\[\\]",
-                     "\\[pp?\\.?\\s*[0-9]+"),
-              line,
-              perl = TRUE)){
-      message(line)
-      stop("Unnecessary p in postnote.")
-    }
-    # Check single hyphen between pagerefs
-    if (grepl("cites?\\[\\]\\[[0-9]+-[0-9]+\\]", line, perl = TRUE)){
-      message(line)
-      stop("Page ranges should be separated by two hyphens (--).")
-    }
+  if (any(grepl(r2("cites?\\[\\]",
+                   "\\[pp?\\.?\\s*[0-9]+"),
+                lines[line_nos_with_cites],
+                perl = TRUE))){
+    line_no <- line_nos_with_cites[grepl(r2("cites?\\[\\]",
+                                           "\\[pp?\\.?\\s*[0-9]+"),
+                                        lines[line_nos_with_cites],
+                                        perl = TRUE)][[1]]
+    context <- lines[[line_no]]
+    .report_error(line_no = line_no,
+                  context = line,
+                  error_message = "Unnecessary p in postnote.")
+    stop("Unnecessary p in postnote.")
+  }
+  # Check single hyphen between pagerefs
+  if (any(grepl("cites?\\[\\]\\[[0-9]+-[0-9]+\\]", lines[line_nos_with_cites], perl = TRUE))){
+    line_no <- line_nos_with_cites[grepl("cites?\\[\\]\\[[0-9]+-[0-9]+\\]",
+                                         lines[line_nos_with_cites],
+                                         perl = TRUE)][[1]]
+    context <- lines[[line_no]]
+    .report_error(line_no = line_no,
+                  context = line,
+                  error_message = "Page ranges should be separated by two hyphens (--).")
+    stop("Page ranges should be separated by two hyphens (--).")
   }
 
   # Check footcites when only one key:
@@ -54,16 +66,14 @@ check_cite_pagerefs <- function(filename, .report_error){
     line_nos <- grep(plural_cite_but_single_key,
                     lines,
                     perl = TRUE)
-    line_no <- line_no[[1]]
+    line_no <- line_nos[[1]]
     context <- lines[[line_no]]
     .report_error(line_no = line_no,
                   context = lines[[line_no]],
-                  error_message = paste0("Use of singular form of textcite or footcite.")
-                  )
+                  error_message = paste0("Use of singular form of textcite or footcite."))
     stop("Use of singular form of \\textcite or \\footcite but { follows immediately after the first key. ",
          "Did you mean \\textcites or \\footcites ?")
   }
-
 
   invisible(NULL)
 }

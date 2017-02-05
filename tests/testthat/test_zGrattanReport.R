b58f8a61041e0c6568a1d6de6b3685beb4ee509e
HughParsonage <hugh.parsonage@gmail.com>
2017-02-06 00:52:59 +1100

    RM dups (oops)

--- tests/testthat/test_zGrattanReport.R
+++ tests/testthat/test_zGrattanReport.R
@@ -17,5 +17,25 @@ test_that("SchoolFunding.tex", {
   checkGrattanReport(path = "./SchoolFunding", compile = TRUE, pre_release = TRUE, release = FALSE)
   
   expect_true(file.exists("./SchoolFunding/PRE-RELEASE/SchoolFunding.pdf"))
-  
 })
+
+test_that("Engaging-students", {
+  skip_on_travis()
+  skip_if_not(file.exists('C:/Program Files/gs/gs9.20/bin/gswin64c.exe'))
+  Sys.setenv(R_GSCMD = 'C:/Program Files/gs/gs9.20/bin/gswin64c.exe')
+  if (!dir.exists("./Engaging-students/RELEASE")){
+    dir.create("./Engaging-students/RELEASE")
+  }
+  
+  if (file.exists("./Engaging-students/RELEASE/Engaging-students--creating-classrooms-that-improve-learning.pdf")){
+    file.remove("./Engaging-students/RELEASE/Engaging-students--creating-classrooms-that-improve-learning.pdf")
+  }
+  
+  checkGrattanReport(path = "./Engaging-students/", compile = TRUE, pre_release = TRUE, release = TRUE)
+  
+  expect_true(file.exists("./SchoolFunding/PRE-RELEASE/SchoolFunding.pdf"))
+  file.remove("./Engaging-students/RELEASE/Engaging-students--creating-classrooms-that-improve-learning.pdf")
+})
+
+
+

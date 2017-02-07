d058a991e424908ad3ece2fe483a321c36623459
HughParsonage <hugh.parsonage@gmail.com>
2017-02-07 17:50:10 +1100

    Je suis HughParsonage != hughparsonage

--- R/checkGrattanReport.R
+++ R/checkGrattanReport.R
@@ -63,7 +63,7 @@ checkGrattanReport <- function(path = ".",
   
   output_method <- match.arg(output_method)
   
-  if (pre_release && update_grattan.cls && !identical(Sys.getenv("TRAVIS_REPO_SLUG"), "hughparsonage/grattex")){
+  if (pre_release && update_grattan.cls && !identical(tolower(Sys.getenv("TRAVIS_REPO_SLUG")), "hughparsonage/grattex")){
     download_failure <- download.file("https://raw.githubusercontent.com/HughParsonage/grattex/master/grattan.cls",
                                       destfile = "grattan.cls",
                                       quiet = TRUE)

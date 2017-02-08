a4cf222c0881f3bff69b6da4f6f7d6c98a23a577
HughParsonage <hugh.parsonage@gmail.com>
2017-02-08 12:28:57 +1100

    No background color for biber warns

--- R/check_log.R
+++ R/check_log.R
@@ -29,10 +29,14 @@ check_log <- function(path = ".", final = FALSE, check_for_rerun_only = FALSE){
   if (any(grepl("undefined references", log_file, fixed = TRUE))){
     which_line <- grep("undefined references", log_file, fixed = TRUE)
     cat(which_line, "\n", log_files[which_line])
+    .report_error(error_message = "Undefined cross-references.", 
+                  advice = "Check each use of Vref and Cref and that it contains a valid \\label.")
     stop("Undefined references.")
   }
 
   if (any(grepl("LaTeX Warning: There were multiply-defined labels.", log_file, fixed = TRUE))){
+    .report_error(error_message = "Multiply-defined labels",
+                  advice = "You have used \\label{} with the same key more than once.")
     stop("LaTeX Warning: There were multiply-defined labels.")
   }
   

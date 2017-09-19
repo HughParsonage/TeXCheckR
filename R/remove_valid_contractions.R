2ecb56725fba675188a8516de3d3609a40d6a0d5
HughParsonage <hugh.parsonage@gmail.com>
2017-09-19 21:23:22 +1000

    Improve performance

--- R/remove_valid_contractions.R
+++ R/remove_valid_contractions.R
@@ -1,7 +1,13 @@
 
 remove_valid_contractions <- function(lines){
-  for (valid in valid_English_contractions){
-    lines <- gsub(sprintf("\\b%s\\b", valid), "<contraction>", lines, perl = TRUE, ignore.case = TRUE)
+  for (valid in valid_English_contractions) {
+    # Note may contain false positives (xisn't) but that's ok
+    # because it's only meant to limit the execution time in the for-loop
+    contain_valid <- grepl(valid, lines, perl = TRUE, ignore.case = TRUE)
+    if (any(contain_valid)) {
+      lines[contain_valid] <- 
+        gsub(sprintf("\\b%s\\b", valid), "<contraction>", lines[contain_valid], perl = TRUE, ignore.case = TRUE)
+    }
   }
   lines
 }

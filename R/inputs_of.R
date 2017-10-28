6a70080ffbcacd7b45c3a5112efba83c5fa434a2
HughParsonage <hugh.parsonage@gmail.com>
2017-10-28 12:11:31 +1100

    Add inputs_of HughParsonage/grattanReporter#73
    Exports inputs of so it can be used in grattanReporter to finish the partition

--- R/inputs_of.R
+++ R/inputs_of.R
@@ -1,7 +1,13 @@
+#' Inputs to files nested within LaTeX document
+#' @param filename The file whose \code{\\inputs} are to be extracted.
+#' @param exclude.preamble (logical) If \code{TRUE}, the default, only \code{\\input}s and \code{\\include}s within the \code{document} environment are returned.
+#' @param append.tex Should the result include the file extension \code{.tex}? By default, \code{TRUE}. Setting to \code{FALSE} may be useful when the file is not a \code{.tex} file.
+#' @return A character vector of file paths relative to \code{filename} that are
+#'  used as \code{\\input}s or \code{\\include}s within \code{filename}. If 
+#'  no such files are present within \code{filename}, \code{NULL} is returned.
+#' @export
 
-
-inputs_of <- function(filename, exclude.preamble = TRUE, append.tex = TRUE){
-  file_path <- dirname(filename)
+inputs_of <- function(filename, exclude.preamble = TRUE, append.tex = TRUE) {
   lines <- read_lines(filename)
   
   if (any(grepl("\\end{document}", lines, fixed = TRUE))) {

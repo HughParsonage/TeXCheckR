b11183a20dfa5175c28fd4084a92b71d05c32515
HughParsonage <hugh.parsonage@gmail.com>
2018-01-26 12:31:45 +1100

    Reduce dep on stri_locate_first_fixed

--- R/suggested_speedups.R
+++ R/suggested_speedups.R
@@ -15,3 +15,32 @@ write_lines <- function(...) {
   }
 }
 
+stri_locate_first_fixed_no_stringi <- function(str, pattern) {
+  out <- matrix(NA_integer_, nrow = length(str), ncol = 2L)
+  rows_with_pattern <- grep(pattern, str, fixed = TRUE)
+  nchar_pattern <- nchar(pattern)
+  for (i in rows_with_pattern) {
+    res <- as.vector(regexpr(pattern = pattern,
+                             text = str[i],
+                             fixed = TRUE),
+                     mode = "integer")
+    out[i, 1L] <- res
+    
+  }
+  out[, 2L] <- out[, 1L] + nchar_pattern - 1L
+  # Conformance with stringi
+  setattr(out, "dimnames", value = list(NULL, c("start", "end")))
+  out
+}
+
+stri_locate_first_fixed <- function(str, pattern, ...) {
+  if (requireNamespace("stringi", quietly = TRUE)) {
+    stringi::stri_locate_first_fixed(str = str, pattern = pattern, ...)
+  } else {
+    stri_locate_first_fixed_no_stringi(str, pattern)
+  }
+}
+
+
+
+
--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -6,3 +6,11 @@ test_that("Seq_union always length one", {
   expect_equal(Seq_union(c(73, 100), c(83, 105)), c(73:83, 100:105))
   expect_equal(Seq_union(c(1, 10), c(9, 19)), 1:19)
 })
+
+
+test_that("stringi replacements", {
+  skip_if_not_installed("stringi")
+  Lines <- readLines("Example-report.Rnw")
+  expect_identical(stri_locate_first_fixed_no_stringi(Lines, " - "), 
+                   stri_locate_first_fixed(Lines, " - "))
+})

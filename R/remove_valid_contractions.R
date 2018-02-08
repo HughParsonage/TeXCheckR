57f0bf64348c35804a155e0911ca06d763401a0c
HughParsonage <hugh.parsonage@gmail.com>
2018-02-08 17:58:29 +1100

    Fix: improve column location in sanitized text
    When a contraction is discarded by `remove_valid_contractions`, the replacement is whitespace of the same width as its replacement so that the location of errors on the same line is correct.

--- R/remove_valid_contractions.R
+++ R/remove_valid_contractions.R
@@ -4,9 +4,10 @@ remove_valid_contractions <- function(lines){
     # Note may contain false positives (xisn't) but that's ok
     # because it's only meant to limit the execution time in the for-loop
     contain_valid <- grepl(valid, lines, perl = TRUE, ignore.case = TRUE)
+    ws_valid <- formatC(" ", width = nchar(valid))
     if (any(contain_valid)) {
       lines[contain_valid] <- 
-        gsub(sprintf("\\b%s\\b", valid), "<contraction>", lines[contain_valid], perl = TRUE, ignore.case = TRUE)
+        gsub(sprintf("\\b%s\\b", valid), ws_valid, lines[contain_valid], perl = TRUE, ignore.case = TRUE)
     }
   }
   lines

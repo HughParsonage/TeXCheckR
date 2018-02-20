dedf69123447056d03eccefb2f485783a02a0c27
HughParsonage <hugh.parsonage@gmail.com>
2018-02-20 13:48:29 +1100

    Add check_unclosed_parentheses and tests

--- R/check_unclosed_parentheses.R
+++ R/check_unclosed_parentheses.R
@@ -1,10 +1,17 @@
+#' Check mismatched parentheses
+#' @noRd
 
-
-check_unclosed_parentheses <- function(tex_lines) {
+check_unclosed_parentheses <- function(filename,
+                                       rstudio = FALSE,
+                                       tex_lines = NULL,
+                                       verbose = FALSE) {
+  if (is.null(tex_lines)) {
+    tex_lines <- read_lines(filename)
+  }
   tex_lines_nchar <- nchar(tex_lines)
   
-  # equations
-  tex_lines_nomath <- gsub("\\\\(|\\\\)", "\\$", tex_lines, perl = TRUE)
+  # equations like \(x^2 + y^2 = h^2\)
+  tex_lines_nomath <- gsub("\\\\\\(|\\\\\\)", "\\$", tex_lines, perl = TRUE)
   
   if (sum(stri_count_fixed(tex_lines, pattern = "(")) != 
       sum(stri_count_fixed(tex_lines, pattern = ")"))) {
@@ -17,13 +24,40 @@ check_unclosed_parentheses <- function(tex_lines) {
     parsed <- 
       data.table(char = tex_line_chars,
                  char_no = seq_along(tex_line_chars),
-                 line_no = rep(seq_along(tex_lines), times = tex_lines_nchar))
+                 line_no = rep(seq_along(tex_lines), times = tex_lines_nchar),
+                 parenthesis_group = final_parentheses)
     
-    char_no <- max(which(and(final_parentheses == last(final_parentheses),
-                             shift(final_parentheses, fill = 0L) == 0L)))
+    if (last(final_parentheses) > 0L) {
+      # Opens
+      char_no <- max(which(and(final_parentheses == last(final_parentheses),
+                               shift(final_parentheses, fill = 0L) == 0L)))
+      stop(parsed[char_no][["line_no"]], " contains parenthesis that does not close.")
+    } else {
+      # Closes
+      wrong_i <- which.min(final_parentheses)
+      wrong_line_no <- parsed[wrong_i][["line_no"]]
+      wrong_char_no <- parsed[wrong_i][["char_no"]]
+      if (verbose) {
+        print(parsed[line_no == wrong_line_no])
+      }
+      column <- nrow(parsed[line_no == wrong_line_no][char_no <= wrong_char_no])
+      context <- paste0(head(parsed[line_no == wrong_line_no][["char"]], 
+                             column + 80L), 
+                        collapse = "")
+      
+      report2console(file = filename,
+                     line_no = wrong_line_no,
+                     column = column,
+                     context = context, 
+                     rstudio = rstudio && interactive(),
+                     error_message = "Unexpected closing parenthesis.",
+                     advice = paste("Did you forget to open a parenthesis matching this one?",
+                                    "or did you insert this parenthesis accidentally?"))
+    }
     
     
-    stop(parsed[char_no][["line_no"]], " contains parenthesis that does not close.")
+    
+    
     
   }
 }
--- tests/testthat/test_check_unclosed_parentheses.R
+++ tests/testthat/test_check_unclosed_parentheses.R
@@ -1,10 +1,20 @@
 context("Unclosed parentheses")
 
 test_that("Closed parentheses NULL", {
-  expect_null(check_unclosed_parentheses("This (should) work."))
+  expect_null(check_unclosed_parentheses(tex_lines = "This (should) work."))
 })
 
 test_that("Unclosed parentheses error", {
-  expect_error(check_unclosed_parentheses("(This should error"), 
+  expect_error(check_unclosed_parentheses(tex_lines = "(This should error"), 
                regexp = "parenthesis that does not close")
 })
+
+test_that("Unopened parenthesis", {
+  options("TeXCheckR.halt_on_error" = FALSE)
+  expect_output(check_unclosed_parentheses(tex_lines = "This parenthesis) should have been opened"),
+                regexp = "Unexpected closing parenthesis.")
+  options("TeXCheckR.halt_on_error" = TRUE)
+  expect_error(check_unclosed_parentheses(tex_lines = "This parenthesis) should have been opened"),
+               regexp = "Unexpected closing parenthesis.")
+})
+

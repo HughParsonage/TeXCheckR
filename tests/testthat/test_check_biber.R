44503e1ba0f02cc26842f926ff98a4e70026f63b
HughParsonage <hugh.parsonage@gmail.com>
2018-02-27 23:30:29 +1100

    Use Sys.which location of biber

--- tests/testthat/test_check_biber.R
+++ tests/testthat/test_check_biber.R
@@ -5,8 +5,8 @@ test_that("Couldn't find an entry for", {
   skip_on_cran()
   get_wd <- getwd()
   setwd("check-biber/lost-entry/")
-  invisible(system2(command = "pdflatex", c("-draftmode", "a.tex"), stdout = TRUE))
-  invisible(system2("biber", args = c("--onlylog",  "a"), stdout = TRUE))
+  invisible(system2(command = "pdflatex", c("-draftmode", "-halt-on-error", "a.tex"), stdout = TRUE))
+  invisible(system2(Sys.which("biber"), args = c("--onlylog",  "a"), stdout = TRUE))
   expect_error(check_biber(), "Biber emitted a warning")
   Sys.sleep(0.5)
   # Put this here so different runs of pdflatex don't affect

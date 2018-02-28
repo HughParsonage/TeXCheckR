7c3aea003700da8236dd0ff90606d6bbe75e5fdf
HughParsonage <hugh.parsonage@gmail.com>
2018-02-28 22:22:30 +1100

    Use tinytex conditionally in test

--- tests/testthat/test_check_biber.R
+++ tests/testthat/test_check_biber.R
@@ -5,8 +5,12 @@ test_that("Couldn't find an entry for", {
   skip_on_cran()
   get_wd <- getwd()
   setwd("check-biber/lost-entry/")
-  invisible(system2(command = "pdflatex", c("-draftmode", "-halt-on-error", "a.tex"), stdout = TRUE))
-  tinytex::pdflatex("a.tex", bib_engine = "biber")
+  if (requireNamespace("tinytex", quietly = TRUE) &&
+      nzchar(Sys.which("tlmgr"))) {
+    tinytex::pdflatex("a.tex", bib_engine = "biber")
+  } else {
+    invisible(system2(command = "pdflatex", c("-draftmode", "-halt-on-error", "a.tex"), stdout = TRUE))
+  }
   invisible(system2(Sys.which("biber"), args = c("--onlylog",  "a"), stdout = TRUE))
   expect_error(check_biber(), "Biber emitted a warning")
   Sys.sleep(0.5)

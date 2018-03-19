cc54ddfdfb64a772c07f81ff274b5a6ce1957176
HughParsonage <hugh.parsonage@gmail.com>
2018-03-20 02:11:11 +1100

    Remove Colah) in regex

--- data-raw/put-correctly-spelled-words.R
+++ data-raw/put-correctly-spelled-words.R
@@ -1,9 +1,12 @@
-
+library(magrittr)
+library(readr)
 correctly_spelled_words_txt <- 
   c(readLines("./data-raw/correctly_spelled_words.txt", skipNul = TRUE), 
-    readLines("./data-raw/proper-nouns-1.txt"))
+    read_lines("./data-raw/proper-nouns-1.txt")) %>%
+  unique %>%
+  .[!grepl(")", ., fixed = TRUE)]
 correctly_spelled_words_txt <- correctly_spelled_words_txt[order(correctly_spelled_words_txt)]
-writeLines(correctly_spelled_words_txt, "./data-raw/correctly_spelled_words.txt")
+write_lines(correctly_spelled_words_txt, "./data-raw/correctly_spelled_words.txt")
 
 CORRECTLY_SPELLED_WORDS_CASE_SENSITIVE <-
   grep("[A-Z]", correctly_spelled_words_txt, value = TRUE)
